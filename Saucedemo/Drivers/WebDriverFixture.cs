dcb756201d4fb5a3683350a65c15f66dfdb9a263
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-08-06 21:52:46 +0300

    disable password checkup

--- Saucedemo/Drivers/WebDriverFixture.cs
+++ Saucedemo/Drivers/WebDriverFixture.cs
@@ -4,14 +4,30 @@ using OpenQA.Selenium.Chrome;
 namespace Saucedemo.Drivers
 {
     public class WebDriverFixture : IDisposable
+
     {
         public IWebDriver Driver { get; private set; }
 
         public WebDriverFixture()
         {
-            Driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            var prefs = new Dictionary<string, object>
+        {
+            { "credentials_enable_service", false },
+            { "profile.password_manager_enabled", false }
+        };
+            options.AddArgument("--incognito");
+            options.AddArgument("--disable-features=PasswordManagerEnableMigration");
+            options.AddUserProfilePreference("credentials_enable_service", false);
+            options.AddUserProfilePreference("profile.password_manager_enabled", false);
+
+
+
+            Driver = new ChromeDriver(options);
+
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
         }
 
         public void Dispose()
@@ -19,5 +35,7 @@ namespace Saucedemo.Drivers
             Driver.Quit();
             Driver.Dispose();
         }
+
+
     }
 }
